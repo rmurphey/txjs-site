f68132d411e810911361d2c55e087db1c2b704db
Rebecca Murphey <rebecca@rebeccamurphey.com>
2010-03-19 18:24:20 -0400

    revised layout

---
+++ index.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html> 
+<html lang="en"> 
+<head> 
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+    <meta http-equiv="Content-Style-Type" content="text/css" /> 
+    <link rel="icon" href="favicon.ico" type="image/x-icon" /> 
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" /> 
+    <meta name="author" content="http://yayquery.com/" /> 
+    <title>TXJS | Texas Javascript Conference - June 5th, 2010</title> 
+    <link type="text/css" rel="stylesheet" media="all" href="css/base.css" /> 
+
+	<!--[if IE]>
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head> 
+<body> 
+	<ul id="banner">
+		<li class="news"><strong>News:</strong> Announcing Douglas Crockford and John Resig as speakers!</li>
+		<a href="http://twitter.com/txjs"><li class="twitter">Follow @txjs on Twitter</li></a>
+	</ul>
+	
+	<div id="wrapper">
+	<!-- <nav>
+		<menu>
+			<li><a href="index.php">About</a></li>
+			<li><a href="speakers.php">Speakers</a></li>
+			<li><a href="sponsorship.php">Sponsorship</a></li>
+		</menu>
+	</nav> -->
+
+	<section id="mainbar">
+		<header id="flag"> 
+		    <h1>TXJS</h1> 
+		    <h2>June 5th, 2010</h2> 
+		    <h3>Austin, TX</h3> 
+		</header>
+
+		<section id="content">
+			<header>
+				<h2 class="code">var TXJS = JS + BBQ + you;</h2>
+				<h3>What could be better than that?</h3>
+			</header>
+			
+			<!-- eventbrite iframe -->
+			<div style="height:300px;overflow:hidden;"><iframe src="http://www.eventbrite.com/tickets-external?eid=582129164&ref=etckt" frameborder="0" marginwidth="5" marginheight="5" vspace="0" hspace="0" width="100%" height="300" allowtransparency="true" scrolling="auto"></iframe><a href="http://www.eventbrite.com/r/etckt"><img src="http://www.eventbrite.com/s.gif" alt="Events" border="0"/></a></div>
+			
+			<p class="intro">TXJS is a full-day conference in Austin, Texas. The goal of the conference is to provide a low-cost, high-quality opportunity for more than 150 developers to get access to some of the best minds in JavaScript. </p>
+			<p>Our featured guests so far, in merely alphabetical order:</p>
+			<ul>
+				<li><strong>Brandon Aaron</strong>, jQuery team member</li>
+				<li><strong>Tim Caswell</strong>, Node.js contributor and founder of <a href="http://howtonode.org">howtonode.org</a></li>
+				<li><strong>Douglas Crockford</strong>, JavaScript architect at Yahoo! and author of "JavaScript: The Good Parts"</li>
+				<li><strong>Andrew Dupont</strong>, Prototype core team member</li>
+				<li><strong>Peter Higgins</strong>, Dojo project lead</li>
+				<li><strong>Paul Irish</strong>, jQuery team member and yayQuery podcast co-host</li>
+				<li><strong>Brian LeRoux</strong>, software architect at Nitobi and PhoneGap hacker/contributor</li>
+				<li><strong>Joe McCann</strong>, senior technologist at Frog Design</li>
+				<li><strong>John Resig</strong>, creator of the jQuery JavaScript library</li>
+				<li><strong>Alex Sexton</strong>, front-end developer and yayQuery podcast co-host</li>
+				<li><strong>Kyle Simpson</strong>, <a href="http://twitter.com/AustinJS">Austin JS</a> organizer and author of LABjs</li>
+				<li><strong>Adam Sontag</strong>, jQuery UI developer relations team member and yayQuery podcast co-host</li>
+				<li><strong>Mike Taylor</strong>, HTML5 aficionado and front-end developer at Tunecore</li>
+				<li><strong>Juriy Zaytsev</strong> (aka @kangax), expert JavaScript wrangler and Prototype core developer</li>
+			</ul>
+		
+			<p>Here's the plan:</p>
+
+			<ul>
+				<li>A whole lotta JavaScripters hanging out all day in Austin, Texas</li>
+				<li>Lightning talks &mdash; show us your codez in 5 minutes or less!</li>
+				<li>BBQ! (or  not, if it's not your thing)</li>
+			</ul>
+
+			<p><strong>Follow us at <a href="http://twitter.com/txjs">@txjs</a> to stay in touch</strong>, but register now so you can tell your friends you knew us when.</p>
+			
+		</section>
+	</section>
+
+	<section id="promos">
+		<article>
+			<header>
+				<h4>Hosted by the yayQuery Podcast</h4>
+			</header>
+			<p><a href="http://yayQuery.com"><img src="images/yayQueryLogo.png" alt="yayQuery"></a></p>
+			<p>Saturday, June 5, 9am-6pm<br>
+				Location TBA shortly</p>
+		</article>
+		
+		<!-- <article>
+			<header>
+				<h4>Location</h4>
+			</header>
+			<p>Thompson Conference Center<br>
+			2405 Robert Dedman Drive<br>
+			Austin, TX 78712</p>
+<iframe width="210" height="200" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Thompson+Conference+Center,+2405+Robert+Dedman+Drive,+Austin,+TX+78712&amp;sll=37.0625,-95.677068&amp;sspn=54.79724,115.224609&amp;ie=UTF8&amp;hq=Thompson+Conference+Center,&amp;hnear=2405+Robert+Dedman+Dr,+Austin,+TX+78705&amp;ll=30.288717,-97.728882&amp;spn=0.014823,0.01708&amp;z=14&amp;output=embed"></iframe><br /><small><a href="http://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=Thompson+Conference+Center,+2405+Robert+Dedman+Drive,+Austin,+TX+78712&amp;sll=37.0625,-95.677068&amp;sspn=54.79724,115.224609&amp;ie=UTF8&amp;hq=Thompson+Conference+Center,&amp;hnear=2405+Robert+Dedman+Dr,+Austin,+TX+78705&amp;ll=30.288717,-97.728882&amp;spn=0.014823,0.01708&amp;z=14" style="color:#0000FF;text-align:left">View Larger Map</a></small>		
+		</article> -->
+		
+		<article>
+			<header>
+				<h4>Hack night party,<br> sponsored by Google</h4>
+			</header>
+			<p><a href="http://www.google.com"><img src="http://www.rebeccamurphey.com/stuff/white_online.gif" alt="Google" /></a></p>
+			<p>Can't get enough JavaScript? Join us for a post-conference hack night and party, sponsored by Google. Details to come, but expect many of our great speakers to join us at a venue in downtown Austin.</p>
+		</article>
+		
+		<article class="sponsors">
+			<header>
+				<h4>Amenity Sponsors</h4>
+			</header>
+			<p><a href="http://environmentsforhumans.com/"><img src="/images/e4h.png" alt="Environments for Humans"></a></p>
+		</article>
+
+		<article class="sponsors">
+			<header>
+				<h4>Media Sponsors</h4>
+			</header>
+			<p><a href="http://atxwebshow.com/"><img src="/images/atxwebshow.png" alt="ATX Web Show"></a></p>
+		</article>
+	</section>
+	</div>
+	
+	<div id="parallax"> 
+	    <div id="bluebonnets"><img src="images/bluebonnets.jpg" width="333" height="208" alt="" /></div> 
+	    <div id="cloud1"><img src="images/cloud.png" width="542" height="317" alt="" /></div> 
+	    <div id="cloud2"><img src="images/cloud.png" width="542" height="317" alt="" /></div> 
+	    <div id="cloud3"><img src="images/rainbow_clouds.png" width="386" height="303" alt="" /></div> 
+	</div> 
+	
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.js" type="text/javascript"></script> 
+    <script src="js/jquery.jparallax.js" type="text/javascript"></script> 
+    <script type="text/javascript"> 
+	// Localize
+	window.TX = $;
+	TX(function(){
+	    (function(){
+		var plx = TX('#parallax'),
+		    win = TX(window);
+		plx.width(win.width());
+		plx.height(win.height())
+	    })();
+	    // Parallax
+	    TX('#parallax').jparallax({},
+		{xtravel: '50px', ytravel: '0px'},
+		{xtravel: '200px', ytravel: '90px'},
+		{xtravel: '300px', ytravel: '100px'},
+		{xtravel: '400px', ytravel: '110px'},
+		{xtravel: '0px', ytravel: '0px'}
+	    );
+	    
+	    TX('#cloud1').css({'top':'10%', 'left': '80%'});
+	    TX('#cloud2').css({'top':'50%', 'left': '20%'});
+	    TX('#cloud3').css({'top':'60%', 'left': '75%'});
+	});
+    </script> 
+</body> 
+</html>
